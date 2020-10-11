7b421c3384d48e5aeefe0a0da0f9b2b6761a2606
Joel Ek <contact@joelek.se>
2020-10-11 11:02:51 +0200

    Add test script.

---
+++ test/index.ts
@@ -0,0 +1,40 @@
+import * as libhttp from "http";
+import * as sockets from "../source";
+
+let server = new sockets.WebSocketServer();
+
+server.addEventListener("connect", (message) => {
+	console.log("server connect:", message);
+	server.send(message.connection_id, "server says hi!");
+});
+
+server.addEventListener("disconnect", (message) => {
+	console.log("server disconnect:", message);
+});
+
+server.addEventListener("message", (message) => {
+	console.log("server message:", message);
+	server.close(message.connection_id);
+});
+
+let http = libhttp.createServer(server.getRequestHandler()).listen(80);
+
+let client = new sockets.WebSocketClient("ws://localhost/");
+
+client.addEventListener("close", (message) => {
+	console.log("client close:", message);
+	http.close();
+});
+
+client.addEventListener("error", (message) => {
+	console.log("client error:", message);
+});
+
+client.addEventListener("message", (message) => {
+	console.log("client message:", message);
+});
+
+client.addEventListener("open", (message) => {
+	console.log("client open:", message);
+	client.send("client says hi!")
+});
